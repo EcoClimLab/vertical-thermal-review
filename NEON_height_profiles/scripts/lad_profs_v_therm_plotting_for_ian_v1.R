90608967c029ffacb825f1f264e339c25fe58a9c
Marielle N Smith <45404993+m-n-smith@users.noreply.github.com>
2021-01-30 17:40:35 -0500

    Add files via upload
    R script to plot LAD and light profiles from lidar (NEON AOP) data

---
+++ NEON_height_profiles/scripts/lad_profs_v_therm_plotting_for_ian_v1.R
@@ -0,0 +1,126 @@
+
+################ CODE TO PLOT LAD AND LIGHT PROFILES FOR VERTICAL PROFILES TANSLEY REVIEW ##################
+
+#--Load required packages
+require(ggplot2)
+require(ggsci) # Needed for the scale_color_jco() colour scale
+
+#--Load the data
+setwd("~/Dropbox (MSU Forest-Atmo)/MSU Forest-Atmo Team Folder/Marielle/Collaborations/Vertical_thermal_review/Codes")
+load("lad_n_light_profs_for_ian_v1.RData") # Loads 3 dataframes: LAD.all (LAD profiles), lgt.all (light transmission profs), prop.sun.all (proportion of sun leaves profiles)
+
+
+#>>---------------------------------------------------------------------------------------------------------------------------
+# PLOTTING
+#>>---------------------------------------------------------------------------------------------------------------------------
+
+########## PLOT THE PROFILES ###########
+
+#--LAD scaling factor 
+lad.scale <- 5.5 # I have checked our LAI estimates against published values for SERC, but would be good to check more values 
+
+#--LAD
+plot.dat <- LAD.all
+
+lad.plot <- ggplot(plot.dat, aes(ht, lad*lad.scale, colour=site)) + 
+  geom_path(size=0.7) + coord_flip() +
+  xlim(0,60) + 
+  labs(x="Canopy height (m)", y = expression(Leaf~area~density~(m^{2}~m^{-3}))) +
+  theme_classic(16)  + scale_color_jco() 
+# lad.plot <- lad.plot + theme(legend.position = "none")
+lad.plot
+
+#--Check the LAI values for the sites
+LAD.all <- LAD.all[LAD.all$ht <= 60,]
+sum((LAD.all[LAD.all$site=="SERC","lad"]*lad.scale), na.rm=TRUE)
+sum((LAD.all[LAD.all$site=="HARV","lad"]*lad.scale), na.rm=TRUE)
+sum((LAD.all[LAD.all$site=="PUUM","lad"]*lad.scale), na.rm=TRUE)
+sum((LAD.all[LAD.all$site=="OSBS","lad"]*lad.scale), na.rm=TRUE)
+sum((LAD.all[LAD.all$site=="SCBI","lad"]*lad.scale), na.rm=TRUE)
+sum((LAD.all[LAD.all$site=="WREF","lad"]*lad.scale), na.rm=TRUE)
+
+
+ggsave(filename="LAD_profs_panel_w_legend_v1.png", width=5, height=7, dpi=200)
+# ggsave(filename="LAD_profs_panel_no_legend_v1.png", width=4, height=7, dpi=200)
+
+
+#--LAD normalised by max height
+plot.dat <- LAD.all
+plot.dat$ht.norm <- plot.dat$ht/(plot.dat$max.ht)
+
+lad.plot <- ggplot(plot.dat, aes(ht.norm, lad*lad.scale, colour=site)) + 
+  geom_path(size=0.7) + coord_flip() +
+  xlim(0,1) +
+  labs(x="Scaled canopy height", y = expression(Leaf~area~density~(m^{2}~m^{-3}))) +
+  theme_classic(16)  + scale_color_jco() 
+# lad.plot <- lad.plot + theme(legend.position = "none")
+lad.plot
+
+
+#--Light profiles
+plot.dat <- lgt.all[lgt.all$ht>5,]  # Exclude bottom 5 m
+
+lgt.plot <- ggplot(plot.dat, aes(ht, lgt, colour=site)) + 
+  geom_path(size=0.7) + coord_flip() +
+  xlim(0,60) + 
+  labs(x="Canopy height (m)", y = 'Proportion incident light') +
+  theme_classic(16)  + scale_color_jco() 
+# lgt.plot <- lgt.plot + theme(legend.position = "none")
+lgt.plot
+
+
+
+##-Height normalised light profiles
+plot.dat <- lgt.all[lgt.all$ht>5,]  # Exclude bottom 5 m
+plot.dat$ht.norm <- plot.dat$ht/(plot.dat$max.ht)
+
+lgt.plot <- ggplot(plot.dat, aes(ht.norm, lgt, colour=site)) + 
+  geom_path(size=0.7) + coord_flip() +
+  xlim(0,1) + 
+  labs(x="Scaled canopy height (m)", y = 'Proportion incident light') +
+  theme_classic(16)  + scale_color_jco() 
+lgt.plot <- lgt.plot + theme(legend.position = "none")
+lgt.plot
+
+
+#--Proportion of layer 1 (sun) LAD
+plot.dat <- prop.sun.all[prop.sun.all$ht>5,]  # Exclude bottom 5 m 
+
+# Exclude second 1.0 prop from df
+all.ones <- which(plot.dat$lad==1)
+not.these <- c(2,4,6,8,10,12)
+exclude <- all.ones[not.these]
+
+plot.dat <- plot.dat[-exclude,]
+
+sun.prop.plot <- ggplot(plot.dat, aes(ht, lad, colour=site)) + 
+  geom_path(size=0.7) + 
+  coord_flip() +
+  xlim(0,60) + 
+  labs(x="Canopy height (m)", y = "Proportion sun leaves") +
+  theme_classic(16)  + scale_color_jco() 
+#sun.prop.plot <- sun.prop.plot + theme(legend.position = "none")
+sun.prop.plot
+
+
+#--Proportion of layer 1 (sun) LAD - height normalised
+plot.dat <- prop.sun.all[prop.sun.all$ht>=5,] 
+plot.dat$ht.norm <- plot.dat$ht/(plot.dat$max.ht)
+
+# Exclude second 1.0 prop from df
+all.ones <- which(plot.dat$lad==1)
+not.these <- c(2,4,6,8,10,12)
+exclude <- all.ones[not.these]
+
+plot.dat <- plot.dat[-exclude,]
+
+sun.prop.plot <- ggplot(plot.dat, aes(ht.norm, lad, colour=site)) + 
+  geom_path(size=0.7) + 
+  coord_flip() +
+  xlim(0,1) + 
+  labs(x="Scaled canopy height (m)", y = "Proportion sun leaves") +
+  theme_classic(16)  + scale_color_jco() 
+# sun.prop.plot <- sun.prop.plot + theme(legend.position = "none")
+sun.prop.plot
+
+
